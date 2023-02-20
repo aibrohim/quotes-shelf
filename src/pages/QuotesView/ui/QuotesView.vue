16a3b3f6397cbe5b70d4d07efb49dbfa003508e8
Ibrohim <ibrohim7840@gmail.com>
2023-02-20 10:24:21 +0500

    fixed some bugs

--- src/pages/QuotesView/ui/QuotesView.vue
+++ src/pages/QuotesView/ui/QuotesView.vue
@@ -1,14 +1,17 @@
 <script>
+import { onMounted } from "vue";
+
 import FilterQuotes from "@/widgets/FilterQuotes/ui/FilterQuotes.vue";
 import QuotesList from "@/widgets/QuotesList/ui/QuotesList.vue";
 
 import { store } from "@/shared/store";
-import { onMounted } from "vue";
+import LoadingSpinner from "@/shared/ui/LoadingSpinner.vue";
 
 export default {
   components: {
     FilterQuotes,
     QuotesList,
+    LoadingSpinner,
   },
   setup() {
     onMounted(() => {
@@ -21,7 +24,8 @@ export default {
 </script>
 
 <template>
-  <div class="row">
+  <LoadingSpinner v-if="$store.state.loading" />
+  <div v-else class="row">
     <div class="col-12 col-lg-3 mb-3">
       <FilterQuotes />
     </div>
